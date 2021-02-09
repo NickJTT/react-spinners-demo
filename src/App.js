afc437448c19d199dacfb8169d56f70c97b4404f
NickJTT <nickjtt357@gmail.com>
2021-02-09 10:11:35 +0200

    Implement the React Spinners demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
+import { ClipLoader } from 'react-spinners';
 
-function App() {
+export default function App() {
+  // loading = { true }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = { style.app }>
+      <ClipLoader loading color = 'blue' size = '64px'/>
     </div>
   );
 }
-
-export default App;
